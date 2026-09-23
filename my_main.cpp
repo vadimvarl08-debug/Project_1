4caf0611c903ddd12d194e4e5844b036db45e325
VadimV <Vadim.vip100@mail.ru>
2026-09-23 18:16:34 +0300

    Add a and b (int and float)

--- my_main.cpp
+++ my_main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int a = 5;
+float b = 10.67;
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "a = " << a << " b = " << b << std::endl;
+
     return 0;
 }
