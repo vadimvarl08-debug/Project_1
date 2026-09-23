b4bc3c457bc1be99207ae5a90e45efb0cde2f9ec
VadimV <Vadim.vip100@mail.ru>
2026-09-23 18:48:01 +0300

    Add func sum

--- my_main.cpp
+++ my_main.cpp
@@ -8,9 +8,16 @@ void print(int a, float b)
     std::cout << "a = " << a << " b = " << b << std::endl;
 }
 
+int sum(int a, int b)
+{
+    return a + b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     print(a, b);
+
+    std::cout << "Сумма 6 и 20 равна = " << sum(6, 20) << std::endl;
 
     return 0;
 }
