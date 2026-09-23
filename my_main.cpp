2830dd9a740e9b9e6eea61755ac33f7baf669865
VadimV <Vadim.vip100@mail.ru>
2026-09-23 18:21:00 +0300

    Add func print()

--- my_main.cpp
+++ my_main.cpp
@@ -3,6 +3,11 @@
 int a = 5;
 float b = 10.67;
 
+void print(int a, float b)
+{
+    std::cout << "a = " << a << " b = " << b << std::endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "a = " << a << " b = " << b << std::endl;
